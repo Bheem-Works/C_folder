1c14040f007de7f12522900a65c3d7678011a9a7
bheem-works <shokokomichan098@gmail.com>
2026-01-15 10:15:08 +0545

    Halfway calculator

---
+++ First_week/calculator.c
@@ -0,0 +1,39 @@
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+
+    char userInput[40];
+
+    printf("Do you want to enter the calc world : (y/n)\n: ");
+
+    scanf("%s", userInput);
+
+    printf(" The user say's ['%s']\n", userInput);
+
+    if (strcmp(userInput, "y") == 0)
+    {
+        printf("okay you are entering the game");
+    }
+    else
+    {
+        printf("Sorry you are not allowed");
+    }
+
+    int a, b;
+
+    printf("Enter the a : ");
+    scanf("%d", &a);
+    printf("Enter the b : ");
+    scanf("%d", &b);
+
+    // printing the variable number;
+    printf("%d", a);
+    printf("%d", b);
+    return 0;
+};
+
+// Note : In the Comparing we use the strcmp;
+// And strcmp return the 0 for true and the 1 for the false. Basically it returns the answer in the boolean.

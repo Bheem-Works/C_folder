fb3eea26d005240e9ac26806ba4cc054394443e6
bheem-works <shokokomichan098@gmail.com>
2026-01-21 20:37:57 +0545

    The first commit ("Student mark manager set up");

---
+++ Second_week/student-mark-manager.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+
+    while (1)
+    {
+        printf("=====================");
+        printf("STUDENTS MARKS MANAGER");
+        printf("======================");
+
+        printf("1. Add student marks");
+        printf("2. View student marks");
+        printf("3. Calculate Result");
+        printf("4. exit");
+
+        int choice;
+        printf("Enter your choice:_");
+        scanf("%d", &choice);
+    }
+}

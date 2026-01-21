ac33dc3e6f3b299d69533bf0edc54dcef6baf79b
bheem-works <shokokomichan098@gmail.com>
2026-01-21 20:58:23 +0545

    Finsihing the First Task in the c

--- Second_week/student-mark-manager.c
+++ Second_week/student-mark-manager.c
@@ -5,16 +5,51 @@ int main()
     while (1)
     {
         printf("=====================");
-        printf("STUDENTS MARKS MANAGER");
-        printf("======================");
+        printf("STUDENTS MARKS MANAGER\n");
+        printf("======================\n");
 
-        printf("1. Add student marks");
-        printf("2. View student marks");
-        printf("3. Calculate Result");
-        printf("4. exit");
+        printf("1. Add student marks\n");
+        printf("2. View student marks\n");
+        printf("3. Calculate Result\n");
+        printf("4. exit\n");
+	printf("\n\n");
 
         int choice;
         printf("Enter your choice:_");
         scanf("%d", &choice);
+	
+	if(choice == 1){
+		printf("====================");
+		printf("ADD STUDENTS MARKS");
+		printf("====================");
+		printf("\n\n");
+
+		char studentName[50];
+		printf("Enter students name:");
+		scanf("%s",studentName);
+
+		int rollNo;
+		printf("Enter the roll number:");
+		scanf("%d",&rollNo);
+
+		printf("Enter marks:\n");
+
+		int math,science,english;
+		printf("Math:");
+		scanf("%d",&math);
+
+		printf("Science:");
+		scanf("%d",&science);
+
+		printf("English:");
+		scanf("%d",&english);
+		printf("\n");
+
+		printf("\n\n");
+
+		printf("Student data saved Sucessfully");
+
+	}
     }
+    return 0;
 }

ae6e1ffa76d280e3c3376db430fb8542602b5b07
bheem-works <shokokomichan098@gmail.com>
2026-01-26 13:28:14 +0545

    The first step of the contact book

---
+++ Second_week/contact-book.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+struct contactBook
+{
+    char name[50];
+    int
+};
+
+int main()
+{
+
+    while (1)
+    {
+        printf("==========");
+        printf("CONTACT BOOK");
+        printf("=========");
+        printf("\n\n");
+
+        printf("1.Add new contact\n");
+        printf("2.View all contact\n");
+        printf("3.Search contact\n");
+        printf("4.Delete contact\n");
+        printf("5.Exit\n\n");
+
+        printf("Enter your choice_:");
+    }
+
+    return 0;
+}

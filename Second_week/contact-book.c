b30116b8e82af61d28d1ddd1c6168a9e1ea34930
bheem-works <shokokomichan098@gmail.com>
2026-01-26 14:00:58 +0545

    Two step finished

--- Second_week/contact-book.c
+++ Second_week/contact-book.c
@@ -4,7 +4,7 @@
 struct contactBook
 {
     char name[50];
-    int phoneNumber[40];
+    char phoneNumber[40];
     char email[50];
 };
 
@@ -12,7 +12,9 @@ int main()
 
 {
 
-    struct contactBook cb;
+    struct contactBook cb[100];
+    int count = 0;
+    int choice;
 
     while (1)
     {
@@ -27,17 +29,41 @@ int main()
         printf("4.Delete contact\n");
         printf("5.Exit\n\n");
 
-        int choice;
         printf("Enter your choice_:");
         scanf("%d", &choice);
 
         if (choice == 1)
         {
+            // Entering the name;
+            printf("Enter The name : ");
+            scanf("%s", cb[count].name);
+
+            // Enter the phoneNumber;
+            printf("Enter the phone number :");
+            scanf("%s", cb[count].phoneNumber);
+
+            // Entr the email;
+            printf("Enter the Email :");
+            scanf("%s", cb[count].email);
+
+            // Incrementing the count;
+            count++;
+            printf("Contact added successfully\n");
         }
-        else if ()
+        else if (choice == 2)
         {
-        };
+            printf("--contact list\n --");
+            for (int i = 0; i < count; i++)
+            {
+                printf("number:%d \n Name:%s \nPhoneNumber: %s\n Email: %s\n", i + 1, cb[i].name, cb[i].phoneNumber, cb[i].email);
+            }
+        }
+        else if (choice == 5)
+        {
+            printf("Thanks for using the contact book");
+            break;
+        }
     }
 
     return 0;
-}
+};

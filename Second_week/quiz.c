2b4c9bb3580141e6ffb0ab149ff8737216e12029
bheem-works <shokokomichan098@gmail.com>
2026-01-30 18:25:57 +0545

    The quiz sections is completed

--- Second_week/quiz.c
+++ Second_week/quiz.c
@@ -1,59 +1,63 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <conio.h>
 
 int main()
 {
 
-    char question[][100] = {
-        "1. What is the capital of France?\n(a) Berlin\n(b) Madrid\n(c) Paris\n(d) Rome\n",
-        "2. Which planet is known as the Red Planet?\n(a) Earth\n(b) Mars\n(c) Jupiter\n(d) Saturn\n",
-        "3. Who wrote 'Hamlet'?\n(a) Charles Dickens\n(b) Mark Twain\n(c) William Shakespeare\n(d) Jane Austen\n"};
-    char answers[] = {'c', 'b', 'c'};
+    char question[][100] = {">>>   1. Who is the founder of Microsoft? ",
+                            ">>>   2. Who is considered the father of the World Wide Web? ",
+                            ">>>   3. What programming language was developed by James Gosling at Sun Microsystems? "};
 
-    int numberOfQuestions = sizeof(question) / sizeof(question[0]); // Used to find the length of the questions.
+    char Options[][100] = {"          A. Steve Jobs", "          B. Bill Gates", "          C. Mark Zuckerberg", "          D. Larry Page",
+                           "          A. Bill Gates", "          B. Tim Berners-Lee", "          C. Steve Jobs", "          D. Larry Page",
+                           "          A. Java", "          B. Python", "          C. Ruby", "          D. C#"};
 
-    printf("Number of Questions: %d\n", numberOfQuestions);
+    char answer[3] = {'B', 'B', 'A'};
+
+    int numberofquestions = sizeof(question) / sizeof(question[0]);
 
     char guess;
-
     int score = 0;
 
     printf("\n\n");
-    printf("-------------------\n");
-    printf("   QUIZ TIME!   \n");
-    printf("-------------------\n\n");
+    printf("****************************************************\n");
+    printf("           Welcome to the quiz game!\n");
+    printf("****************************************************\n\n");
 
-    for (int i = 0; i < numberOfQuestions; i++)
+    for (int i = 0; i < numberofquestions; i++)
     {
         printf("%s\n", question[i]);
         printf("\n");
 
-        for (int j = (i * 4); j < (i * 4); j++)
+        for (int j = (i * 4); j < (i * 4) + 4; j++)
         {
-            printf("%s\n", answers[j]);
+            printf("%s\n", Options[j]);
             printf("\n");
         }
 
-        printf("Enter your answer :");
-        scanf("%s", &guess);
+        printf("Enter your answer: ");
+        scanf(" %c", &guess);
         printf("\n");
+
         guess = toupper(guess);
 
-        if (guess == answers[i])
+        if (guess == answer[i])
         {
-            printf(" Correct! \n");
+            printf(">> Correct!\n");
             printf("\n");
             score++;
         }
         else
         {
-            printf("Wrong\n");
+            printf("\033[22;31m>> Wrong!\033[0m\n");
+
+            printf("\n");
         }
     }
-
-    printf("--------------------\n");
-    printf("Your score is : %d out of %d.\n", score, numberOfQuestions);
-    printf("--------------------\n");
-
+    printf("****************************************************\n");
+    printf("Your score is %d out of %d.\n", score, numberofquestions);
+    printf("****************************************************\n");
+    printf("\n\n");
     return 0;
 };

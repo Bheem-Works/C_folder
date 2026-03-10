40cd8447f2af3da2ef32fa56b9cdbacdb2a4c1af
bheem-works <shokokomichan098@gmail.com>
2026-03-10 19:16:59 +0545

    Factorial Number the questions nine is done

---
+++ LabProjects/questionNine.c
@@ -0,0 +1,15 @@
+// write a program to find out the factorial number using recursive functions; 
+
+#include <stdio.h>
+int factorial(int n){
+	if (n == 0) return 1; 
+	n * factorial(n - 1);
+};
+
+int main(){
+	int num;
+	printf("Enter the Number : ");
+	scanf("%d",&num);
+	printf("The factorial number is%d:\n",factorial(num));
+	return 0;
+};

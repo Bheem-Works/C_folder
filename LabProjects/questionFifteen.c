e59a5a3b6b2e37ea2a2d42de4cdb7bb0ce428700
bheem-works <shokokomichan098@gmail.com>
2026-03-12 19:35:01 +0545

    Question Fifteen (swapp)

---
+++ LabProjects/questionFifteen.c
@@ -0,0 +1,18 @@
+// Write a program to swap the numbers by using the pointer; 
+//
+
+#include <stdio.h>
+int swap (int *a,int *b){
+	int temp = *a;
+	*a = *b ;
+	*b = temp;
+};
+
+int main(){
+	int x,y;
+	printf("Enter a number:");
+	scanf("%d%d",&x,&y);
+	swap(&x,&y);
+	printf("After swap : %d %d",x,y);
+	return 0;
+};

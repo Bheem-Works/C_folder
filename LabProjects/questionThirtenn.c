a19008ffef7b844926a4f9db7ae51f290ce7a2a3
bheem-works <shokokomichan098@gmail.com>
2026-03-12 19:06:02 +0545

    The sum of the numbers by using the pointers

---
+++ LabProjects/questionThirtenn.c
@@ -0,0 +1,14 @@
+
+// Questions Thirteen questions : Write a programs to calculate sum of two numbers by using the pointer;
+
+#include <stdio.h>
+
+int main(){
+	int a = 3; 
+	int b = 4; 
+	int *first = &a;
+	int *second = &b;
+	int sum = *first + *second;
+	printf("The sum %d",sum);
+	return 0;
+};

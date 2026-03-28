f242f4acd952a98d0ede2f9e907ad01e32f55b4e
bheem-works <shokokomichan098@gmail.com>
2026-03-28 08:40:54 +0545

    Fix Fibonacci series input and output handling

--- LabProjects/fibo.c
+++ LabProjects/fibo.c
@@ -1,23 +1,39 @@
 #include <stdio.h>
+
 int main()
 {
-
     int i, n;
     int a = 0;
     int b = 1;
-    int c = a + b;
+    int c;
 
-    printf("Enter the numbers of term");
-    scanf("%d", &n);
-    printf("Fibonacci Series: %d, %d, ", t1, t2);
+    printf("Enter the number of terms: ");
+    if (scanf("%d", &n) != 1 || n <= 0)
+    {
+        printf("Please enter a positive integer.\n");
+        return 1;
+    }
+
+    printf("Fibonacci Series: ");
+
+    if (n >= 1)
+    {
+        printf("%d", a);
+    }
+
+    if (n >= 2)
+    {
+        printf(", %d", b);
+    }
 
     for (i = 3; i <= n; i++)
     {
-        printf("%d", c);
+        c = a + b;
+        printf(", %d", c);
         a = b;
         b = c;
-        c = a + b;
-    };
+    }
 
+    printf("\n");
     return 0;
 }
